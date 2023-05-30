6c4e92a8e6d23b8652e8d0d0d2ddddfa0df0b934
Rupesh1439 <rupesh.shrestha96742@gmail.com>
2023-05-30 14:31:55 +0545

    feat: change base url

--- app/services/apiClient.ts
+++ app/services/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://localhost:1337/api", // TODO Update this url
+  baseURL: "https://sunbla.azurewebsites.net/api", // TODO Update this url
   headers: {
     "Content-type": "application/json",
     Authorization:
-      "Bearer 8910053e18cd2837aa4f27d2740f1b26850689c31d06a835061245858b7902443140c76ea9088b224e4137c977f4269c04aeb711e425a336b3c31c42cfeef969ddd1ebb66f8a3f566edd698f1956695e00c356628e3e8253e342c8ff395603a5f3f2151db2257b9106adaa9065458d35d8b386efa1c05f53865b61e557179603",
+      "Bearer 4c3ee091c06c07dd2b3e7898ad768227680d31c7e7ddd9969efc4a64a4d8214f3363bdd910aa5f1b44d15071d96f73fec144f712b5b729d323ea2a19efe7cc79a1c3157d63e4816864cbcd1c41482ee5b80c2fe008ca6a407740b3e625388373858806119fe029420ab57609587dc0553e1ae54305efe3067377ba587a8eafd4",
   },
 })
