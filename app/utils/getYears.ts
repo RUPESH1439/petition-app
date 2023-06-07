e3ffb7a206d890b5168f6e1c6bf62078aead0829
Rupesh1439 <rupesh.shrestha96742@gmail.com>
2023-06-07 14:54:20 +0545

    fix: year

--- app/utils/getYears.ts
+++ app/utils/getYears.ts
@@ -1,4 +1,4 @@
 const currentYear = new Date().getFullYear()
 const range = (start, stop, step) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
-export const getYearRange = () => range(currentYear, currentYear - 50, -1)
+export const getYearRange = () => range(currentYear, currentYear - 200, -1)
